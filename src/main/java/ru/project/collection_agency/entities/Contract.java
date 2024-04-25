368aac19e7a1e0fd46c7fefc80fe51af391e09bd
wafflelios <107104736+wafflelios@users.noreply.github.com>
2024-04-25 17:52:48 +0500

    Update Contract.java

--- src/main/java/ru/project/collection_agency/entities/Contract.java
+++ src/main/java/ru/project/collection_agency/entities/Contract.java
@@ -20,4 +20,48 @@ public class Contract
     private Date date;
 
     private String conditions;
+
+    public Contract(Long id, Long debtorId, Long debtId, Date date, String conditions) {
+        this.id = id;
+        this.debtorId = debtorId;
+        this.debtId = debtId;
+        this.date = date;
+        this.conditions = conditions;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getDebtorId() {
+        return debtorId;
+    }
+
+    public void setDebtorId(Long debtorId) {
+        this.debtorId = debtorId;
+    }
+
+    public Long getDebtId() {
+        return debtId;
+    }
+
+    public void setDebtId(Long debtId) {
+        this.debtId = debtId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getConditions() {
+        return conditions;
+    }
+
+    public void setConditions(String conditions) {
+        this.conditions = conditions;
+    }
 }
